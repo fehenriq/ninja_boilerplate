efb7ea08271a731e439e4fe20fb1cb14bd252a58
fehenriq <lipe.h.r@gmail.com>
2024-12-26 16:28:53 -0300

    :test_tube: test: add test configs

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.getenv("DEBUG"))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = str(os.getenv("ALLOWED_HOSTS")).split(" ")
 
 
 # Application definition

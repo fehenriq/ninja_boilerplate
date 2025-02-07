4bfa9da06e837b7769f4914a3daae047ff3796ea
fehenriq <lipe.h.r@gmail.com>
2025-02-07 16:22:27 -0300

    :sparkles: feat: add GIMIxService and ErrorSchema for API interactions

---
+++ utils/gimix_service.py
@@ -0,0 +1,31 @@
+import uuid
+
+import requests
+
+
+class GIMIxService:
+    def __init__(self):
+        self.base_url = "https://gimix-api.vercel.app/api"
+
+    def send_request(self, endpoint: str, method: str, token: str):
+        url = f"{self.base_url}/{endpoint}"
+        headers = {"Authorization": f"Bearer {token}"}
+
+        response = requests.request(method, url, headers=headers, timeout=10)
+        response.raise_for_status()
+        json_response = response.json()
+
+        return json_response
+
+    def get_user_email_by_id(self, user_id: uuid.UUID, token: str):
+        endpoint = f"users/{user_id}"
+        response = self.send_request(endpoint, "GET", token)
+        json_response = response.json()
+
+        return json_response["email"]
+
+    def get_app_admins_email(self, token: str):
+        endpoint = "users/emails?is_app_admin=true"
+        response = self.send_request(endpoint, "GET", token)
+
+        return response
